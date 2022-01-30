bc9974e3378112178b08f86cbb127234a4b4735a
suxinde2009 <suxinde2009@126.com>
2022-01-30 21:48:11 +0800

    update

--- utils/time/time.go
+++ utils/time/time.go
@@ -1,7 +1,15 @@
 package time
 
-import "time"
+import (
+	"strconv"
+	"time"
+)
 
 func NowInMilli() int64 {
 	return time.Now().UnixNano() / 1000000
+}
+
+func GetCurrentTimestampSec() int {
+	ts, _ := strconv.Atoi(strconv.FormatInt(time.Now().UnixNano()/1000000000, 10))
+	return ts
 }
