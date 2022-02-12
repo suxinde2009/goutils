26d9501067ec252b230df7aed7a5e8d13c41a019
suxinde2009 <suxinde2009@126.com>
2022-02-12 21:53:42 +0800

    update

--- collection/set.go
+++ collection/set.go
@@ -1,36 +1,71 @@
 package collection
 
-type IntegerSet map[int64]struct{}
+import "sync"
 
-func NewIntegerSet() IntegerSet {
-	return make(map[int64]struct{})
+type Set struct {
+	m   map[interface{}]struct{}
+	len int
+	sync.RWMutex
 }
 
-func (set IntegerSet) Add(v int64) {
-	if _, ok := set[v]; ok {
+func NewSet(cap int64) *Set {
+	temp := make(map[interface{}]struct{}, cap)
+	return &Set{
+		m: temp,
+	}
+}
+
+func (s *Set) Add(item interface{}) {
+	s.Lock()
+	defer s.Unlock()
+
+	s.m[item] = struct{}{}
+	s.len = len(s.m)
+}
+
+func (s *Set) Remove(item interface{}) {
+	s.Lock()
+	defer s.Unlock()
+
+	if s.len == 0 {
 		return
 	}
-	set[v] = struct{}{}
+
+	delete(s.m, item)
+	s.len = len(s.m)
 }
 
-func (set IntegerSet) Contains(v int64) bool {
-	if _, ok := set[v]; ok {
-		return true
-	}
-	return false
+func (s *Set) Has(item interface{}) bool {
+	s.RLock()
+	defer s.RUnlock()
+
+	_, ok := s.m[item]
+	return ok
 }
 
-func (set IntegerSet) Remove(v int64) {
-	if _, ok := set[v]; !ok {
-		return
-	}
-	delete(set, v)
+func (s *Set) Len() int {
+	return s.len
 }
 
-func (set IntegerSet) Clone() IntegerSet {
-	n := NewIntegerSet()
-	for k, v := range set {
-		n[k] = v
+func (s *Set) IsEmpty() bool {
+	return s.Len() == 0
+}
+
+func (s *Set) Clear() {
+	s.Lock()
+	defer s.Unlock()
+
+	s.m = make(map[interface{}]struct{})
+	s.len = 0
+}
+
+func (s *Set) List() []interface{} {
+	s.RLock()
+	defer s.RUnlock()
+
+	list := make([]interface{}, 0, s.len)
+	for item := range s.m {
+		list = append(list, item)
 	}
-	return n
+	return list
 }
--- collection/set_test.go
+++ collection/set_test.go
@@ -5,7 +5,34 @@ import (
 	"testing"
 )
 
-func Test(t *testing.T) {
+func TestSet(t *testing.T) {
+	s := NewSet(5)
+
+	s.Add(1)
+	s.Add(2)
+	s.Add(1)
+
+	fmt.Println("list of all items in set: ", s.List())
+
+	s.Clear()
+	if s.IsEmpty() {
+		fmt.Println("The set is cleared, now is empty")
+	}
+
+	s.Add(1)
+	s.Add(2)
+	s.Add(3)
+
+	if s.Has(2) {
+		fmt.Println("2 exists in the set")
+	}
+
+	s.Remove(2)
+	s.Remove(3)
+	fmt.Println("list of all items in set: ", s.List())
+}
+
+func TestIntegerSet(t *testing.T) {
 	intSet := NewIntegerSet()
 
 	intSet.Add(333)
